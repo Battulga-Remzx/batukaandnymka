0223e9fd602450f10c03335b0f78732021407294
Battulga-Remzx <bayarbaatarbattulga5959@gmail.com>
2024-09-06 00:44:03 +0800

    commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var countDownDate = new Date("Oct 13, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = now - countDownDate;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById('countdown').innerHTML = days + 'хон ';
+  document.getElementById('countdown').innerHTML += hours + 'цаг ';
+  document.getElementById('countdown').innerHTML += minutes + 'мин ';
+  document.getElementById('countdown').innerHTML += seconds + 'сек ';
+}, 1000).get
---
+++ secret/index.js
@@ -0,0 +1,5 @@
+let login = prompt('Enter Password');
+
+if(login !== '221013') {
+  window.location.href = "https://www.example.com";
+}
